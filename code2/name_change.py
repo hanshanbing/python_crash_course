ed252917e125efcf587e0c26437abdb738959c76
hanshanbing <2514808325@qq.comom>
2018-11-08 14:18:52 +0800

    添加练习

---
+++ code2/name_change.py
@@ -0,0 +1,12 @@
+﻿#调整名字的大小写
+first_name = "Han"
+last_name = "shanbing"
+
+#小写输出
+print(first_name.lower( ) + " " + last_name.lower( ))
+
+#首字母大写输出
+print(first_name.title( ) + " " + last_name.title( ))
+
+#大写输出
+print(first_name.upper( ) + " " + last_name.upper( ))
