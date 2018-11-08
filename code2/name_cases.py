e85e22ecec042e307f6ebdc26a684d9702b968be
hanshanbing <2514808325@qq.comom>
2018-11-08 14:04:53 +0800

    修改

--- code2/name_cases.py
+++ code2/name_cases.py
@@ -1,4 +1,4 @@
 #p23 动手试一试 2-3
 first_name = "han"
-message = "hello " + first_name + ",would you like to learn some python today"
+message = "hello " + first_name + ",would you like to learn some python today？"
 print(message)
