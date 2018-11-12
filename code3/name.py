39c0278acee302454bf8a7d627bc3d2af4a7914a
hanshanbing <2514808325@qq.comom>
2018-11-12 16:36:53 +0800

    添加

---
+++ code3/name.py
@@ -0,0 +1,5 @@
+﻿# 3-1 姓名 列表练习
+names = ["zhou ze","hao shaohua","ding yupeng"]
+print(names[0].title())
+print(names[1].title())
+print(names[-1].title())
---
+++ code3/vehicle.py
@@ -0,0 +1,4 @@
+﻿#3-3
+vehicles = ["Honda","BMW"]
+print("I would like to own a " + vehicles[0] + " motorcyle")
+print("I would like to own a " + vehicles[1] + " motorcyle")
