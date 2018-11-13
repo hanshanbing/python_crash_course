383835a653b359a8214bd9eca4bbad2dee2fd895
hanshanbing <2514808325@qq.comom>
2018-11-13 18:52:44 +0800

    添加第6章内容

---
+++ code4/alien.py
@@ -0,0 +1,23 @@
+﻿#P94 创建字典列表
+
+#创建一个用于存储外星人的空列表
+aliens = [ ];
+
+#创建30个绿色的外星人
+for alien_number in range(30):
+	new_alien = {'color':'green','points':5,'speed':'slow'}
+	aliens.append(new_alien)
+
+#改变前三个外星人的值
+for alien in aliens[:3]:
+	alien['color'] = 'yellow'
+	alien['points'] = 10
+	alien['speed'] = 'fast'
+
+#显示前五个外星人
+for alien in aliens[:5]:
+	print(alien)
+print('........')
+
+#显示创建了多少个外星人
+print("Total number of aliens: " + str(len(aliens)))
---
+++ code4/people.py
@@ -0,0 +1,9 @@
+﻿#6-1 人
+people = {'first_name' : 'liu', 
+	'last_name' : 'wenjing', 
+	'age' : 25, 
+	'city' : 'donghai' ,}
+print(people['first_name'])
+print(people['last_name'])
+print(people['age'])
+print(people['city'])
